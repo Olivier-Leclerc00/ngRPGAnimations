738bcbc3450f8e899cbe0886fab6f7498779c645
Olivier <2087689@cegepmontpetit.ca>
2024-11-01 15:55:20 -0400

    FCT Animations
    Boucle infini qui fait tourner le jelly. Suite de trois animations: bounce, shake et flip en utilisant des timer async.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {transition, trigger, useAnimation} from "@angular/animations";
-import { jello, pulse, shakeX } from 'ng-animate';
+import { bounce, flip, jello, pulse, shakeX } from 'ng-animate';
+import { lastValueFrom, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,8 @@ import { jello, pulse, shakeX } from 'ng-animate';
     trigger('death', [transition(':increment', useAnimation(shakeX, {params: {timing: 1}}))]),
     trigger('attack', [transition(':increment', useAnimation(pulse, {params: {timing: 0.3, scale:4.5}}))]),
     trigger('preAttack', [transition(':increment', useAnimation(jello, {params: {timing: 1}}))]),
+    trigger('bounce', [transition(':increment', useAnimation(bounce, {params: {timing: 1}}))]),
+    trigger('flip', [transition(':increment', useAnimation(flip, {params: {timing: 0.75}}))]),
   ]
 })
 export class AppComponent {
@@ -17,9 +20,13 @@ export class AppComponent {
   css_hit = false;
   css_bounce = false;
   css_vibrate = false;
+  css_rotateC = false;
+  css_rotateHT = false;
   ng_death = 0;
   ng_attack = 0;
   ng_preAttack = 0;
+  ng_bounce = 0;
+  ng_flip = 0;
 
   constructor() {
   }
@@ -46,7 +53,10 @@ export class AppComponent {
     this.ng_preAttack++;
     setTimeout(() => {
       this.ng_attack++;
-      setTimeout(() => this.css_vibrate = true, 600);
+      setTimeout(() => {
+        this.css_vibrate = true
+        setTimeout(() => this.css_vibrate = false, 200);
+      }, 600);
     }, 600);
     
     //setTimeout(() => this.css_vibrate = false, 600);
@@ -56,6 +66,36 @@ export class AppComponent {
     // TODO Utilisé Animista pour faire une animation différente avec css (wobble)
     this.css_hit = true;
     setTimeout(() => this.css_hit = false, 600);
+  }
+
+  async bounceShakeFlip(){
+    // TODO Utilisé Animista pour faire une animation différente avec css (wobble)
+    this.ng_bounce++;
+    await lastValueFrom(timer(1000));
+    this.ng_death++;
+    await lastValueFrom(timer(750));
+    this.ng_flip++;
+  }
+
+  infiniteSpin(){
+    // TODO Utilisé Animista pour faire une animation différente avec css (wobble)
+    this.rotateCenter();
+  }
+
+  rotateCenter(){
+    this.css_rotateC = true;
+    setTimeout(() => {
+      this.css_rotateC = false;
+      this.rotateTop();
+    }, 800);
+  }
+
+  rotateTop(){
+    this.css_rotateHT = true;
+    setTimeout(() => {
+      this.css_rotateHT = false;
+      this.rotateCenter();
+    }, 700);
   }
 
   showSlime(){
